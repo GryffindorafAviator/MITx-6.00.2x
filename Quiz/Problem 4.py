fa01296e64b41ab9cb728aa5e39268f48a4f5c0c
Flash Wang <phynew0111@gmail.com>
2018-10-11 00:15:21 +0800

    Create Problem 4.py

---
+++ Quiz/Problem 4.py
@@ -0,0 +1,15 @@
+# Problem 4
+# 0.0/20.0 points (graded)
+# Consider a list of positive (there is at least one positive) and negative numbers. 
+# You are asked to find the maximum sum of a contiguous subsequence. For example,
+
+# in the list [3, 4, -1, 5, -4], the maximum sum is 3+4-1+5 = 11
+# in the list [3, 4, -8, 15, -1, 2], the maximum sum is 15-1+2 = 16
+# Write a function that meets the specification below.
+
+# def max_contig_sum(L):
+#     """ L, a list of integers, at least one positive
+#     Returns the maximum sum of a contiguous subsequence in L """
+#     #YOUR CODE HERE
+    
+# Paste your entire function in the box below. Do not leave any print statements.
